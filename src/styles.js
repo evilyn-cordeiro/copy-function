cad992f7077b9908799f8811d2eadd397ec59a7d
Evilyn A Lima <77305238+evilyn-araujo@users.noreply.github.com>
2022-07-29 14:17:01 -0300

    Initial commit

---
+++ src/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 600px;
+  margin: 40px auto;
+  position: relative;
+`;
+
+export const Heading = styled.h1`
+  text-align: center;
+  color: purple;
+`;
+
+export const Input1 = styled.input`
+  height: 50px;
+  width: 100%;
+  padding: 0;
+  font-size: 25px;
+  border: 2px solid purple;
+  border-radius: 4px;
+`;
+export const Input2 = styled.input`
+  height: 50px;
+  width: 100%;
+  padding: 0;
+  font-size: 25px;
+  margin-top: 70px;
+  border: 2px solid purple;
+  border-radius: 4px;
+`;
+
+export const Button = styled.button`
+  padding: 20px;
+  font-size: 20px;
+  position: relative;
+  left: 30%;
+  margin-top: 10px;
+  cursor: pointer;
+  background: purple;
+  color: white;
+  border: none;
+  border-radius: 5px;
+`;
